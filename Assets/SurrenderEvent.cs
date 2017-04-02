1e6684377cedd943125ec14b2835b8a4f59af1bd
kaibo <kaibornemann@yayhoo.de>
2017-04-02 15:55:51 +0200

    cur cur

--- Assets/SurrenderEvent.cs
+++ Assets/SurrenderEvent.cs
@@ -7,13 +7,24 @@ public class SurrenderEvent : AGameEvent
     protected override void _StartEvent()
     {
         GameManager.Instance.onEnemyKilledEvent.AddListener(OnEnemyKilled);
+
+        StartCoroutine(SurrenderEventRoutine());
+    }
+
+    private IEnumerator SurrenderEventRoutine()
+    {
+        float dur = Random.Range(minDur, maxDur);
+
+        yield return new WaitForSeconds(dur);
+
+        EventManager.Instance.EndEvent(this);
     }
 
     private void OnEnemyKilled(Health h, Health.EventInfo i)
     {
         if(i.source && i.source.ThePlayer)
         {
-            GameManager.Instance.AddPlayerPoints(h.RootActor.PointsOnKill * -2, i.source.ThePlayer);
+            GameManager.Instance.AddPlayerPoints(-3 * h.RootActor.PointsOnKill, i.source.ThePlayer);
         }
     }
 
